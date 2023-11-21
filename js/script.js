3fda74f698761effc2311124fed0a05aaf65d523
grossiValentina <grossivalentina435@gmail.com>
2023-11-21 16:35:51 +0100

    add: stampa 10 mail in pagina

--- js/script.js
+++ js/script.js
@@ -3,20 +3,28 @@ const { createApp } = Vue;
 const app = createApp({
     data(){
         return{
-            emailRandom: "",
+            emailRandom: [],
 
         }
     },
+    
     methods: {
         generateEmail(){
-            axios
+            for (let i = 0; i < 10; i++) {
+
+                axios
+
                  .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                 
                  .then((resp) => {
                     console.log(resp.data.response);
-                    this.emailRandom = resp.data.response;
+                    this.emailRandom.push(resp.data.response);
                  });
-        }
+               
+                
+            }
+        },
 
-    }
+    },
     
 }).mount("#app");
